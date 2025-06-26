8fb9f1d6e9eb339b3f69555fffb5b957881b4b33
JJMunera <jumunerap@unal.edu.co>
2025-06-26 17:46:16 -0500

    intento1
    a

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,9 +5,50 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 # pylint: disable=import-outside-toplevel
 
 
+import pandas as pd
+import re
+
 def pregunta_01():
+    with open("files/input/clusters_report.txt", 'r', encoding='utf-8') as f:
+        lineas = [line.rstrip() for line in f if line.strip() and not re.match(r'^-+$', line.strip())]
+
+    datos = []
+    i = 0
+    while i < len(lineas):
+        linea = lineas[i]
+
+        if re.match(r'^\s*\d+', linea):
+            partes = re.split(r'\s{2,}', linea.strip(), maxsplit=3)
+            cluster = int(partes[0])
+            cantidad = int(partes[1])
+            porcentaje = float(partes[2].replace(',', '.').replace('%', '').strip())
+            palabras = partes[3] if len(partes) > 3 else ''
+            
+            i += 1
+            while i < len(lineas) and not re.match(r'^\s*\d+', lineas[i]):
+                palabras += ' ' + lineas[i].strip()
+                i += 1
+            
+            palabras = re.sub(r'\s+', ' ', palabras)
+            palabras = re.sub(r',\s*', ', ', palabras)
+            palabras = re.sub(r'\s*,', ',', palabras)
+            palabras = palabras.strip().rstrip(',')
+
+            datos.append({
+                'cluster': cluster,
+                'cantidad_de_palabras_clave': cantidad,
+                'porcentaje_de_palabras_clave': porcentaje,
+                'principales_palabras_clave': palabras
+            })
+        else:
+            i += 1
+
+    return pd.DataFrame(datos)
+
+
+
     """
-    Construya y retorne un dataframe de Pandas a partir del archivo
+      
     'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
 
     - El dataframe tiene la misma estructura que el archivo original.
